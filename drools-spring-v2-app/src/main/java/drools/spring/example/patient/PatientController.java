82efd2742838aecdce1231d3b9767597c3d65a17
Nebojsa Horvat <horvatnebojsa@yahoo.com>
2018-06-12 19:54:58 +0200

    before project version change

---
+++ drools-spring-v2-app/src/main/java/drools/spring/example/patient/PatientController.java
@@ -0,0 +1,35 @@
+package drools.spring.example.patient;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+
+	@Autowired
+	private PatientService patientService;
+	
+//	@GetMapping
+//	public ResponseEntity<List<Patient>> getPatients(){
+//		List<Patient> patients = patientService.findAll();
+//		if(patients == null || patients.isEmpty())
+//			return new ResponseEntity<List<Patient>>(HttpStatus.NOT_FOUND);
+//		return new ResponseEntity<List<Patient>>(patients,HttpStatus.OK);
+//	}
+//	
+//	@GetMapping("/{id:\\d+}")
+//	public ResponseEntity<Patient> getPatient(@PathVariable Long id){
+//		Patient patient = patientService.findOne(id);
+//		if(patient == null)
+//			return new ResponseEntity<Patient>(HttpStatus.NOT_FOUND);
+//		return new ResponseEntity<Patient>(patient,HttpStatus.OK);
+//	}
+}
